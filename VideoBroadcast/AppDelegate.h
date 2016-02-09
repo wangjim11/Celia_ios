64f3e53170fa91c80271b7d836e52bb6a84105dd
Lin Zhou <zhoulin0522@gmail.com>
2016-02-08 22:10:00 -0500

    Initial Commit

---
+++ VideoBroadcast/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VideoBroadcast
+//
+//  Created by Lin Zhou on 2/8/16.
+//  Copyright © 2016 Lin Zhou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ VideoBroadcast/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  VideoBroadcast
+//
+//  Created by Lin Zhou on 2/8/16.
+//  Copyright © 2016 Lin Zhou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
