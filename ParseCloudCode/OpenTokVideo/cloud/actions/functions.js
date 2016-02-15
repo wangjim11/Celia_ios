b54f0c1154e61cf2c6319de17cdf0ae609ca00de
Lin Zhou <zhoulin0522@gmail.com>
2016-02-14 21:54:40 -0500

    Add a new API endpoint to create new cameras.

--- ParseCloudCode/OpenTokVideo/cloud/actions/functions.js
+++ ParseCloudCode/OpenTokVideo/cloud/actions/functions.js
@@ -1,8 +1,38 @@
 var opentok = require("cloud/opentok/opentok.js").createOpenTokSDK("45491612", "0f2e91414959869b342abbebba4f747d55cd9fc3");
 
-Parse.Cloud.define("createRobot", function(request, response) {
-	
+
+Parse.Cloud.define("createCamera", function(request, response) {
+    var user = request.user;
+    if (!user) {
+        response.error("Login before creating camera");
+        return;
+    }
+        
+    var camera = new Parse.Object("Camera");
+    
+    var cameraName = request.params.name;
+    if (cameraName) {
+        camera.set("name", cameraName);
+    }
+
+    var cameraUID = request.params.iosUID;
+    if (cameraUID) {
+        camera.set("iosUID", cameraUID);
+    }
+    
+  	opentok.createSession(function(err, sessionId) {
+		if (err) { 
+			response.error("could not create opentok session for camera: " + camera.id); 
+			return;
+		}		
+		camera.set("optSessionID", sessionId);
+		
+		camera.save().then(function(camera) {
+			response.success(camera);
+    	});
+	}); 	
 });
+
 
 Parse.Cloud.define("getOpentokToken", function(request, response) {
 	var cameraId = request.params.camera;
